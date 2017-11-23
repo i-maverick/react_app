0be8581b4468170d2470efff13133cea63a3ddbe
i-maverick <in79@mail.ru>
2017-11-23 19:45:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,108 @@
-class InputName extends React.Component {
-  render() {
-    return <input type='text' placeholder='enter your name'/>
+class SearchBar extends React.Component {
+	constructor(props) {
+  	super(props)
+    this.handleChange = this.handleChange.bind(this)
   }
-}
 
-class HelloMessage extends React.Component {
-  static propTypes = { name = React.PropTypes.string }
-  static defaultProps = { name = 'Stranger' }
-  
-  render() {
-    return (
-      <div>
-        // <InputName />
-        <div>Hello {this.state.name}</div>
-      </div>
+	handleChange() {
+  	this.props.onUserInput(
+    	this.filterTextInput.value,
+      this.stockOnlyInput.checked
     )
   }
 
-  onChangeHandler(e) {
-    console.log(e.target.value)
+  render() {
+    return (
+      <form>
+        <input type="text" placeholder="Search..."
+          value={this.props.filterText}
+          ref={(input) => this.fieldTextInput = input}
+          onChange={this.handleChange} />
+        <p>
+          <input type="checkbox"
+            checked={this.props.stockOnly}
+            ref={(input) => this.stockOnlyInput = input}
+            onChange={this.handleChange} />
+              &nbsp;Show only stocked products
+        </p>
+      </form>
+    );
   }
 }
 
-class Main extends React.Component {
-  render() {
-    return <HelloMessage />
+class ProductRow extends React.Component {
+	render() {
+  	return (
+    	<tr>
+        <td>{this.props.product.name}</td>
+        <td>{this.props.product.price}</td>
+        <td>{this.props.product.stocked ? 'yes' : 'no'}</td>
+      </tr>
+    )
   }
 }
+
+class ProductTable extends React.Component {
+	render() {
+  	var rows = []
+    this.props.products.forEach((product) => {
+    	if (product.name.indexOf(this.props.filterText) === -1 ||
+      	!product.stocked && this.props.stockOnly) {
+      	return
+      }
+			rows.push(<ProductRow product={product} key={product.name} />)
+    })
+  	return (
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+            <th>In stock</th>
+          </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </table>
+    )
+  }
+}
+
+class FilterableProductTable extends React.Component {
+	constructor(props) {
+  	super(props)
+    this.state = {
+    	filterText: '',
+      stockOnly: false,
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar
+          filterText={this.state.filterText}
+          stockOnly={this.state.stockOnly}
+        />
+        <ProductTable
+          products={this.props.products}
+          filterText={this.state.filterText}
+          stockOnly={this.state.stockOnly}
+        />
+      </div>
+    );
+  }
+}
+
+const products = [
+  {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
+  {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
+  {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
+  {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
+  {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
+  {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
+];
 
 ReactDOM.render(
-  <Main />,
-  document.getElementById('root')
-)
+  <FilterableProductTable products={products} />,
+  document.getElementById('app')
+);
